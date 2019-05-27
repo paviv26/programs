ea2d43dab696ab3e856602d320ee8c3eceb82123
paviv26 <43065903+paviv26@users.noreply.github.com>
2019-05-27 20:42:22 +0530

    Create year.py

---
+++ year.py
@@ -0,0 +1,12 @@
+y=int(input())
+if y%4==0:
+    if y%100!=0:
+       print("yes")
+    else:
+        if y%400==0:
+          print("yes")
+        else:
+          print("no")
+else:
+    print("no")
+    
