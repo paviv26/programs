8161c895716a832881ff3ef864aa09a375a82d8f
paviv26 <43065903+paviv26@users.noreply.github.com>
2019-05-30 23:55:10 +0530

    Update primee.py

--- primee.py
+++ primee.py
@@ -1,8 +1,10 @@
 p,k=map(int,input().split())
-for i in range(p+1,k):
+l=list()
+for i in range(p,k+1):
   count=0
   for j in range(1,i+1):
     if i%j==0:
       count=count+1
   if count==2: 
-      print(i)
+      l.append(i)
+print(len(l))
