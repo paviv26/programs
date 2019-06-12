25fd91bb34d85760efb192d7addda66147b04ea3
paviv26 <43065903+paviv26@users.noreply.github.com>
2019-06-12 18:32:20 +0530

    Create roman.py

---
+++ roman.py
@@ -0,0 +1,5 @@
+str1=input()
+l=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for i in range(len(l)):
+    if l[i]==str1:
+        print(i)
