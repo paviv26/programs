5f3b849b6072281c2196fb09fbd87b6e0f6db34e
paviv26 <43065903+paviv26@users.noreply.github.com>
2019-06-12 23:55:54 +0530

    Update line.py

--- line.py
+++ line.py
@@ -1,7 +1,9 @@
 p,q=map(int,input().split())
 r,s=map(int,input().split())
 t,u=map(int,input().split())
-if p==r==t or q==s==u:
+if p==q and r==s and t==u:
+    print("yes")
+elif p==r==t or q==s==u:
     print("yes")
 else:
     print("no")
