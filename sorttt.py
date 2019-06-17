eeef486a40ade37514e3595f6f4eb41892c3bd5a
paviv26 <43065903+paviv26@users.noreply.github.com>
2019-06-17 21:10:56 +0530

    Create sorttt.py

---
+++ sorttt.py
@@ -0,0 +1,7 @@
+n=int(input())
+l=list(map(str,input().split()))
+l.sort(key=len)
+for i in range(len(l)-1):
+    if len(l[i])==len(l[i+1]) and l[i]>l[i+1]:
+        l[i],l[i+1]=l[i+1],l[i]
+print(l)
